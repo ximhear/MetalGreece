2962e7b6ce6c4de423b8ea6013842fc345528028
C.H Lee <ximhear@gmail.com>
2024-12-12 04:25:57 +0900

    .

---
+++ ShadowMap/ShadowMap/ShadowMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadowMapApp.swift
+//  ShadowMap
+//
+//  Created by gzonelee on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ShadowMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
